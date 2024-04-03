a0d80791b557dce52e8d8ba270b437a146d29165
Imad Zalmat <imaden.zalmat@gmail.com>
2024-04-03 15:01:50 -0700

    Return to previous code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:}".format(element), end="")
             counter += 1
-            if counter = x:
+            if counter >= x:
                 break
         except:
             pass
