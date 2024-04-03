741f33470daad9fb70288701424e462b829efefd
Imad Zalmat <imaden.zalmat@gmail.com>
2024-04-03 00:34:50 -0700

    adjust format specifier to handle different types

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     counter = 0
     for element in my_list:
         try:
-            print("{:d}".format(element), end="")
+            print("{:}".format(element), end="")
             counter += 1
             if counter >= x:
                 print()
