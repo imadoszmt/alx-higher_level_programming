1cae036c06b1fbd9435563408dadebcd299a5a13
Imad Zalmat <imaden.zalmat@gmail.com>
2024-04-03 14:26:49 -0700

    Shadow commands for test

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if not my_list:
-        return 0
+   # if not my_list:
+       # return 0
 
     counter = 0
     for element in my_list:
