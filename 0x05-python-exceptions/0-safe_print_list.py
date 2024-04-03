830ed02d3a057322922ad5bd8637ff6a3c3719f1
Imad Zalmat <imaden.zalmat@gmail.com>
2024-04-03 16:16:43 -0700

    Fix out of range list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:}".format(element), end="")
             counter += 1
-            if counter >= x or not my_list[counter]:
+            if counter >= x or not counter < x:
                 break
         except IndexError:
             break
