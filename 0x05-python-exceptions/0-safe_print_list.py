8d73ee355a567a8bae11c3043b89e6e55b1abaa0
Imad Zalmat <imaden.zalmat@gmail.com>
2024-04-03 15:00:16 -0700

    Fix counter condition

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:}".format(element), end="")
             counter += 1
-            if counter >= x:
+            if counter = x:
                 break
-        except IndexError:
+        except:
             pass
     print()
     return counter
