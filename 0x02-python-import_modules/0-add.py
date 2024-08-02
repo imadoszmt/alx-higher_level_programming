75d0248980e004d69fbf6ea570cc4b768a8acbf6
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-02 11:32:00 -0700

    Add guard to avoid execution when importing add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,4 +5,7 @@ from add_0 import add
 a = 1
 b = 2
 
-print("{} + {} = {}".format(a, b, add(a, b), end=' '))
+#print("{} + {} = {}".format(a, b, add(a, b), end=' '))
+
+if __name__ == "__main__":
+        print("{} + {} = {}".format(a, b, add(a, b), end=' '))
