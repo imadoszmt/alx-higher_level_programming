68f916c1f08e7ce4f9e29e11332285dc63ddefe6
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-03 08:27:14 -0700

    Add infinite command line argument adder program

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    adder = 0
+    for arg in sys.argv[1:]:
+        adder = adder + int(arg)
+        
+    print(adder)
+
+
+if __name__ == "__main__":
+    main()
