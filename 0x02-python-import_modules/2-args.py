e7ef9578769d58f43a335c282760ae5b4f14a6c3
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-02 20:49:20 -0700

    Add guard condition of main function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,16 @@
 
 import sys
 
-if len(sys.argv) == 1:
-    print('0 arguments.')
-elif len(sys.argv) == 2:
-    print('1 argument:')
-    print(f'1: {sys.argv[1]}')
-else:
-    print(f'{len(sys.argv) - 1} arguments:')
-    for arg in sys.argv[1:]:
-        print(f'{sys.argv.index(arg)}: {arg}')
+def main():
+
+    if len(sys.argv) == 1:
+        print('0 arguments.')
+    elif len(sys.argv) == 2:
+        print('1 argument:')
+        print(f'1: {sys.argv[1]}')
+    else:
+        print(f'{len(sys.argv) - 1} arguments:')
+        for arg in sys.argv[1:]:
+            print(f'{sys.argv.index(arg)}: {arg}')
+if __name__ == "__main__":
+    main()
