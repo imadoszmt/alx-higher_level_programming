6112d495ff3d8855de330b981333a57305c26338
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-04 09:57:55 -0700

    Add program that handle basic operation on terminal while importing basic functions needed for that from a module

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+
+    if len(sys.argv) == 4:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == '+':
+            print(f'{a} + {b} = {add(a, b)}')
+        elif sys.argv[2] == '-':
+            print(f'{a} - {b} = {sub(a, b)}')
+        elif sys.argv[2] == '*':
+            print(f'{a} * {b} = {mul(a, b)}')
+        elif sys.argv[2] == '/':
+            print(f'{a} / {b} = {div(a, b)}')
+        else:
+            print('Unkown operator. Available operators: +, -, * and /')
+    else:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        sys.exit(1)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
