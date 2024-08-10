2f8184c068094a4b694e89f84da1c55d114089e3
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-10 11:57:33 -0700

    Add format as a decimal specifier

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@
 def print_reversed_list_integer(my_list=[]):
     list_len = len(my_list) - 1
     for num in my_list:
-        print("{}".format(my_list[list_len]))
+        print("{:d}".format(my_list[list_len]))
         list_len -= 1
