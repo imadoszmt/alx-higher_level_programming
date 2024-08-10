4593b0e0308452afa161d62f943b3d42194a80a5
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-10 14:22:00 -0700

    Fix pycodestyle using "is not" for condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list != None:
+    if my_list is not None:
         list_len = len(my_list) - 1
         for num in my_list:
             print("{:d}".format(my_list[list_len]))
