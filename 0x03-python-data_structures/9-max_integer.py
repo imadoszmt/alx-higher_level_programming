29f23c120d33be4be9809e3f72c6d4795c5bce99
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-11 11:20:48 -0700

    Assign max_num first element of the list for the purpose of tracking instead of zero

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    max_num = 0
+    max_num = my_list[0]
     if len(my_list) != 0:
         for num in my_list:
             if max_num < num:
                 max_num = num
+
         return max_num
     else:
         return None
-
