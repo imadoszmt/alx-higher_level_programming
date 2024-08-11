a6dfa2ff83d625db263034933cd8f1684cefb07d
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-10 17:52:08 -0700

    Fix format specifier

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for inner_mx in matrix:
         for element in inner_mx:
-            print(element, end=' ')
+            print("{:d}".format(element), end=' ')
         print()
