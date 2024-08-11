2e590b56273db76ccab3c1d6f805271246af315d
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-10 18:21:14 -0700

    Print element using "unpacking operator *" to avoid extra space at end to each printed inner list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,5 @@
 
 def print_matrix_integer(matrix=[[]]):
     for inner_mx in matrix:
-        for element in inner_mx:
-            print("{:d} ".format(element), end='')
+        print("{:d} {:d} {:d}".format(*inner_mx), end='')
         print()
