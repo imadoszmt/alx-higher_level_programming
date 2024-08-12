7f9591cc69f73ef81d05ffc6cdf88ce143a8144f
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-12 16:57:44 -0700

    Add program that print a dictionary by sorted key

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_dic = dict(sorted(a_dictionary.items()))
+    for key, value in sorted_dic.items():
+        print("{}: {}".format(key, value))
