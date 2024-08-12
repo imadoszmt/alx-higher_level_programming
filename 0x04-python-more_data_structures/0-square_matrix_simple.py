0c208c3fdf4cdc35c7ce2265b1de30114715ad05
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-12 12:15:52 -0700

    Fix pycodestyle by eleminating whitespace before colon

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@
 def square_matrix_simple(matrix=[]):
     squared_matrix = []
     for inner_matrix in matrix:
-        squared_inner_matrix = list(map(lambda x : x**2, inner_matrix))
+        squared_inner_matrix = list(map(lambda x: x**2, inner_matrix))
         squared_matrix.append(squared_inner_matrix)
-    return squared_matrix 
+    return squared_matrix
