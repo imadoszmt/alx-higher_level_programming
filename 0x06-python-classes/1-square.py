15f76fcee848915daa72c1966cf73f619b24fbd6
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-20 18:47:09 -0700

    Add program that define square class and initiate its instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module define a square shape class
+"""
+
+
+class Square:
+    """
+    This is a class that represent a square shape.
+    
+    Attributes:
+        __size: Private instance attribute of square's size.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the square class.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
