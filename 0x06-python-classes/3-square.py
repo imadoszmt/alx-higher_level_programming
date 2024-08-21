f645c35b244115d29f1ba804ad42b0cc7ae54b30
Imad Zalmat <imaden.zalmat@gmail.com>
2024-08-20 19:47:20 -0700

    Add forgeted conditions for Type and Value error

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -19,6 +19,11 @@ class Square:
         Args:
             size: The size of the square.
         """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
         self.__size = size
 
     def area(self):
